468ba85a6414b31b8e7480141dff6776d2a2dba7
whitewizardd <leumasre@gmail.com>
2024-07-04 20:22:26 +0100

    loadede

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,7 @@ export class AppController {
   ): Promise<object> {
     const ipAddress =
       request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    console.log(ipAddress);
     return this.app_service.get_ip_address_and_location_and_temperature(
       ipAddress[0],
       visitor_name,
