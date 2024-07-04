79c271a1eb5a288925dfc8808d3bf9e1f9965d5e
whitewizardd <leumasre@gmail.com>
2024-07-04 21:38:57 +0100

    locate...

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ip_info_url } from './dto/request_api';
+import {
+  ip_info_url,
+  weather_url,
+  convert_temperature_to_celsius_from_kelvin,
+} from './dto/request_api';
 import axios from 'axios';
 
 @Injectable()
@@ -14,11 +18,21 @@ export class AppService {
   ): Promise<object> => {
     console.log(ip_address);
     const location = (await axios.get(ip_info_url(ip_address))).data;
-    console.log(location);
+    const temp = (await axios.get(weather_url(location.city))).data.main.temp;
+    const temperature: string = convert_temperature_to_celsius_from_kelvin(+temp);
+
     return {
       client_ip: ip_address,
       location: location.city,
-      greeting: `Hello ${visitor_name}`,
+      greeting: generate_greeting(visitor_name, temperature, location.city),
     };
   };
 }
+
+const generate_greeting = (
+  visitor_name: string,
+  temp: string,
+  location: string,
+): string => {
+  return `Hello, ${visitor_name}!, the temperature is ${temp} degrees Celcius in ${location}`;
+};
--- src/dto/request_api.ts
+++ src/dto/request_api.ts
@@ -1,3 +1,10 @@
 export const ip_info_url = (ip_address: string): string => {
   return `http://ip-api.com/json/${ip_address}`;
 };
+
+export const convert_temperature_to_celsius_from_kelvin = (
+  temp_value: number,
+): string => (temp_value - 273.15).toFixed();
+
+export const weather_url = (location: string): string =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHERMAP_API_KEY}`;
